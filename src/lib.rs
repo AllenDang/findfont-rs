e3f050ad8b8a3ad097cc0781795204ac48f410cc
Programmer001 <petko.hupka@gmail.com>
2025-05-01 12:45:12 +0200

    exposing more of the API and adding brief comments with examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod findfont;
 
 pub use findfont::find;
+pub use findfont::get_sys_font_dir;
 pub use findfont::get_sys_font_dirs;
 
 #[cfg(test)]
@@ -8,10 +9,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let font_name = "STHeiti";
+    fn find_works() {
+        let font_name = "Arial";
         let font = findfont::find(font_name);
 
+        println!("Font path: {}", font.clone().unwrap().display());
         assert!(font.is_some());
     }
+
+	#[test]
+	fn get_sys_font_dir_works() {
+		let dir = findfont::get_sys_font_dir();
+
+		println!("Font path: {}", dir.clone().unwrap().display());
+		assert!(dir.is_some());
+	}
 }
